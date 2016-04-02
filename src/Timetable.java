23c95ae5e22c36641597d8c4fc29db90e36276e8
JSDD8 <jonasschloemer@arcor.de>
2016-04-02 16:59:55 +0200

    Added test functionality for getEventsByDate()

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,4 +1,4 @@
-import javax.swing.JFrame;
+import java.util.LinkedList;
 
 public class Timetable {
 	private static int year;
@@ -9,7 +9,7 @@ public class Timetable {
 	public static void main(String[] args){
 		//===================PVZ kaip veikia time klase===============
 			year = time.getYear();
-			month = time.getMonth();
+			month = time.getMonth() + 1;
 			day = time.getDay();
 			System.out.println(year+" "+month+" "+day);
 			
@@ -18,6 +18,36 @@ public class Timetable {
 			year = time.getYear();
 			day = time.getDay();
 			System.out.println(year+" "+month+" "+day);
+			
+			//Event Debugging | Testing
+			
+			User user = new User("test", "test");
+			Event one = new Event( "test", 2002, 12, 4 );
+			Event two = new Event( "test", 2002, 12, 5 );
+			Event three = new Event( "test", 2002, 11, 4 );
+			Event four = new Event( "test", 2012, 12, 4 );
+			Event five = new Event( "tes1t", 2002, 12, 4 );
+			
+			user.addEvent(one);
+			user.addEvent(two);
+			user.addEvent(three);
+			user.addEvent(four);
+			user.addEvent(five);
+			
+			LinkedList<Event> full = user.getEvents();
+			LinkedList<Event> select = user.getEventsByDate(2002, 12, 4);
+			Event given = null;
+			for( int i = 0; i < full.size(); i++ ) {
+				given = full.get(i);
+				System.out.println(given.getYear() + " " + given.getMonth() + " " + given.getDay() + " (" + given.getName() + ")");
+			}
+			Event given2 = null;
+			System.out.println("Events found when searching for 2002 / 12 / 4:");
+			for( int i = 0; i < select.size(); i++ ) {
+				given2 = select.get(i);
+				System.out.println(given2.getYear() + " " + given2.getMonth() + " " + given2.getDay() + " (" + given2.getName() + ")");
+			}
+				
 		//=============================================================
 	}
 	
