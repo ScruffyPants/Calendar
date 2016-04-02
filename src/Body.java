73be0238780dc9b7360e6b4823abb6cfcb491ae8
ScruffyPants <Vilius.vaiciunas12@gmail.com>
2016-04-02 23:20:01 +0300

    Alot of things happened lets not talk about it

--- src/Body.java
+++ src/Body.java
@@ -3,15 +3,49 @@ import javax.swing.JFrame;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Body {
-	JPanel pane = new JPanel();
+public class Body extends JFrame {
+	
+	JFrame frame = new JFrame("GridBagLayout");
+	JPanel panel = new JPanel(new GridBagLayout());
+	JPanel calendar = new JPanel(new GridLayout());
+	JMenuBar menuBar = new JMenuBar();
+	GridBagConstraints c = new GridBagConstraints();
+	
 	public static void main(String[] args) {
 		Body frameTabel = new Body();
 	}
+	
 	Body(){
-		pane = new JPanel();
-		pane.setLayout(new GridBagLayout());
-		//pane.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		pane.setVisible(true);
+		
+		DrawMenu();
+		DrawCalendar();
+		
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.FIRST_LINE_START;
+		c.gridx = 0;
+		c.gridy = 0;
+		panel.add(menuBar,c);
+		
+		c.anchor=GridBagConstraints.PAGE_END;
+		panel.add(calendar,c);
+		
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+	}
+	
+	public void DrawMenu(){
+		JMenu Calendar, Account, Info;
+		JMenuItem Exit, Logout;
+		Calendar = new JMenu("Calendar");
+		menuBar.add(Calendar);
+	}
+	
+	public void DrawCalendar(){
+		calendar.setSize(500, 300);
+		JButton button = new JButton("Test");
+		calendar.add(button);
+		calendar.setBackground(Color.black);
 	}
 }
