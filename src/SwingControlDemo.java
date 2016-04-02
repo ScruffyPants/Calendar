933ac1947c041f29c5ce1322565f3f4a51dd41ae
JSDD8 <jonasschloemer@arcor.de>
2016-04-02 21:30:35 +0200

    Added registration GUI

---
+++ src/SwingControlDemo.java
@@ -0,0 +1,81 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class SwingControlDemo {
+    
+   private JFrame mainFrame;
+   private JLabel headerLabel;
+   private JLabel statusLabel;
+   private JPanel controlPanel;
+
+   public SwingControlDemo(){
+      prepareGUI();
+   }
+
+   public static void main(String[] args){
+      SwingControlDemo swingControlDemo = new SwingControlDemo();      
+   }
+
+   private void prepareGUI(){
+      mainFrame = new JFrame("Calendar Alpha");
+      mainFrame.setSize(300,230);
+      mainFrame.setLayout(null);
+      mainFrame.addWindowListener(new WindowAdapter() {
+         public void windowClosing(WindowEvent windowEvent){
+            System.exit(0);
+         }        
+      });    
+      headerLabel = new JLabel("",JLabel.CENTER);        
+      statusLabel = new JLabel("",JLabel.CENTER);   
+      JButton bregister = new JButton("Register");
+      JPanel panel = new JPanel(null);
+      JTextField txuser = new JTextField(15);
+      JPasswordField pass = new JPasswordField(15);
+      txuser.setBounds(70,30,150,20);
+      pass.setBounds(70,65,150,20);
+      bregister.setBounds(90,135,110,20);
+      
+
+      statusLabel.setSize(350,100);
+
+      controlPanel = new JPanel();
+      controlPanel.setLayout(new FlowLayout());
+
+      mainFrame.add(headerLabel);
+      mainFrame.add(controlPanel);
+      mainFrame.add(statusLabel);
+      mainFrame.add(txuser);
+      mainFrame.add(pass);
+      mainFrame.add(bregister);
+      mainFrame.setVisible(true);  
+      headerLabel.setText("Registration"); 
+
+      final DefaultComboBoxModel rankName = new DefaultComboBoxModel();
+
+      rankName.addElement("Student");
+      rankName.addElement("Teacher");
+      rankName.addElement("Admin");
+
+      final JComboBox rankCombo = new JComboBox(rankName);    
+      rankCombo.setSelectedIndex(0);
+      rankCombo.setBounds(95,100,100,20);
+
+      JScrollPane rankListScrollPane = new JScrollPane(rankCombo);    
+      
+
+      JButton registerButton = new JButton("Register");
+
+      registerButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) { 
+        	 User created = new User(txuser.getText(),pass.getText());
+        	 
+            }              
+      }); 
+      controlPanel.add(rankListScrollPane);          
+      controlPanel.add(registerButton);
+      mainFrame.add(rankCombo);
+      mainFrame.setVisible(true);             
+   }
+
+}
