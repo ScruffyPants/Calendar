b884d0fb61dfcdac20ff2fb895d772b45e7d31d6
ScruffyPants <Vilius.vaiciunas12@gmail.com>
2016-04-05 23:11:16 +0300

    Added group.java

---
+++ src/Group.java
@@ -0,0 +1,26 @@
+import java.io.Serializable;
+import java.util.LinkedList;
+
+public class Group implements Serializable{
+	private String name;
+	private LinkedList<User> users = new LinkedList<User>();
+	private LinkedList<Event> events = new LinkedList<Event>();
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LinkedList<User> getUsers() {
+		return users;
+	}
+	public void setUsers(LinkedList<User> users) {
+		this.users = users;
+	}
+	public LinkedList<Event> getEvents() {
+		return events;
+	}
+	public void setEvents(LinkedList<Event> events) {
+		this.events = events;
+	}
+}
